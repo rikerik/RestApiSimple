7b83a3bbc7c55337dfffe95b97abb874b4b89245
Erik <bako.erik.dominik@gmail.com>
2024-08-02 18:01:50 +0200

    Testing api key

--- simpleapi/src/main/java/restapi/Main.java
+++ simpleapi/src/main/java/restapi/Main.java
@@ -1,7 +1,29 @@
 package restapi;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
+
+    public static final String KEY;
+
+    static {
+        Properties p = new Properties();
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            p.load(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        KEY = p.getProperty("api.key");
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(KEY);
     }
 }
