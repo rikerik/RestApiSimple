776b8e090ce2229af7b678c202e5537d010f7240
Erik <bako.erik.dominik@gmail.com>
2024-08-02 19:02:36 +0200

    Working

--- simpleapi/src/main/java/restapi/Main.java
+++ simpleapi/src/main/java/restapi/Main.java
@@ -4,8 +4,21 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Properties;
 
+import com.google.gson.Gson;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
     public static final String KEY;
@@ -23,7 +36,48 @@ public class Main {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(KEY);
+    public static void main(String[] args) throws Exception {
+
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://github.com/johnmarty3/JavaAPITutorial/raw/main/Thirsty.mp4");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript);
+
+        log.info(jsonRequest);
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", KEY)
+                .POST(BodyPublishers.ofString(jsonRequest))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse = httpClient.send(postRequest, BodyHandlers.ofString());
+
+        log.info(postResponse.body());
+
+        transcript = gson.fromJson(postResponse.body(), Transcript.class);
+
+        transcript.getId();
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
+                .header("Authorization", KEY)
+                .build();
+
+        while (true) {
+            HttpResponse<String> getResponse = httpClient.send(getRequest, BodyHandlers.ofString());
+            transcript = gson.fromJson(getResponse.body(), Transcript.class);
+            log.info(transcript.getStatus());
+
+            if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        log.info("Transcripton completed");
+        log.info(transcript.getText());
+
     }
 }
---
+++ simpleapi/src/main/java/restapi/Transcript.java
@@ -0,0 +1,12 @@
+package restapi;
+
+import lombok.Data;
+
+@Data
+public class Transcript {
+    private String audio_url;
+    private String id;
+    private String status;
+    private String text;
+
+}
